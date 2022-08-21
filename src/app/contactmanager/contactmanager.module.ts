a3b81fa1ec1daa97791384f045517913316b3d45
Milos Mancic <miolos@gmail.com>
2022-08-21 23:42:37 +0200

    inicijalni

---
+++ src/app/contactmanager/contactmanager.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ContactmanagerAppComponent } from './contactmanager-app.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { MainContentComponent } from './component/main-content/main-content.component';
+import { SidenavComponent } from './component/sidenav/sidenav.component';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NotesComponent } from './component/notes/notes.component';
+import { NewContactDialogComponent } from './component/new-contact-dialog/new-contact-dialog.component';
+
+const routes: Routes = [
+  { path: '', component: ContactmanagerAppComponent,
+    children: [
+      { path: ':id', component: MainContentComponent },
+      { path: '', component: MainContentComponent }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  declarations: [
+    ContactmanagerAppComponent,
+    ToolbarComponent,
+    MainContentComponent,
+    SidenavComponent,
+    NotesComponent,
+    NewContactDialogComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ],
+  providers: [
+    UserService
+  ]
+})
+export class ContactmanagerModule { }
